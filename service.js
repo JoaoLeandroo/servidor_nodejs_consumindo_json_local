fd5816c1d217df5ef33efd128fe6b4f38a9c5dc6
joaoleandro <new.joaoleandro@gmail.com>
2024-02-24 13:56:16 -0300

    consumindo arquivo json localmente

---
+++ service.js
@@ -0,0 +1,31 @@
+// categories -> burguers / drinks / lanches / combos
+
+// burguers -> description / value / name / ingredients / urlImg
+
+// drinks -> name / description / value / ingredients / urlImg
+
+const express = require("express");
+const fs = require("fs");
+const server = express();
+
+const loadApi = () => {
+    const dadosApi = "./lanches.json";
+    fs.readFile(dadosApi, "utf8", (err, data) => {
+        if (err) {
+            console.error("Erro ao ler o arquivo JSON:", err);
+            return;
+        }
+
+        const json = JSON.parse(data);
+
+        server.get("/lanches", (req, res) => {
+            return res.json(json.categories);
+        });
+    });
+};
+
+loadApi();
+
+server.listen(3000, () => {
+    console.log("Servidor iniciado na porta 3000");
+});
